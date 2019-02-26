cbd64ef830c7915874cf896d70112bf8c96e28d4
Priya Vyas <pvyas@meddo.in>
2019-02-26 16:53:57 -0500

    Remove devtools

--- src/store.js
+++ src/store.js
@@ -9,11 +9,6 @@ const enhancerArgs = [
     )
 ];
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-if (devTools) {
-    enhancerArgs.push(devTools);
-}
-
 let enhancers = compose(...enhancerArgs);
 
 export const store = createStore(
